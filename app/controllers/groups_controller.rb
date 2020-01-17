cb612108cd842f5fa8f8d3e95b9f9b3c46217c69
yamaden23 <ioiop23n@gmail.com>
2020-01-17 12:41:42 +0900

    editボタンの作成完了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -21,7 +22,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to root_path, notice: 'グループを更新しました'
     else
@@ -32,6 +32,10 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
