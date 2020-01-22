174c91e443f0b4dcabc2fc30e5930054686ce493
yamaden23 <ioiop23n@gmail.com>
2020-01-23 01:56:28 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '3.113.174.155' user: 'yamaden23', roles: %w{app db web}
+server '3.113.174.155' ,user: 'ec2-user', roles: %w{app db web}
