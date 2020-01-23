a8561d7f9db6729b8b7507eb930fdc0ca67d75dd
yamaden23 <ioiop23n@gmail.com>
2020-01-23 21:35:36 +0900

    インクリメンタルサーチ追加修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -23,7 +23,7 @@ $(function() {
       <p class="chat-group-user__name">${name}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
-    $(".js-add-user").append(html);
+    $("#chat-group-users").append(html);
   }
   function addMember(userId) {
     let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
@@ -54,6 +54,7 @@ $(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
   });
+
   $(document).on("click", ".chat-group-user__btn--add", function() {
     
     const userName = $(this).attr("data-user-name");
