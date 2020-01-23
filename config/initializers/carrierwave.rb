2ab01f1481ecfe8ceb3901257d6963b0a7766480
yamaden23 <ioiop23n@gmail.com>
2020-01-23 17:49:01 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,7 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'yamaden23'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yamaden23'
+  config.fog_directory  = 'yamaden2020'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yamaden2020'
+
 end
